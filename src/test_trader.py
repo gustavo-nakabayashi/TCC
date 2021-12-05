429ed4b158d0a616a7689477140ed3937f3dd2c5
Gustavo Barros <gustavobcampos7@gmail.com>
2021-12-05 01:39:53 -0300

    finished implementing stop loss

--- src/test_trader.py
+++ src/test_trader.py
@@ -1,58 +1,48 @@
 import pytest
 from trader import Trader 
 
-NUMBER_1 = 3.0
-NUMBER_2 = 2.0
+min_ann = 2000.0
+max_ann = 3000.0
+stop_loss_percentage = 20
+
+trader = Trader(min_ann, max_ann, stop_loss_percentage)
 
 
 
 def test_buy_signal_on_buy():
     
-    stop_loss_percentage = 20
-    trader = Trader(stop_loss_percentage)
     close = 1000.0
-    min_ann = 2000.0
     is_last_candle_day= False
     
     assert trader.buy_signal (close, min_ann, is_last_candle_day) == True
 
 def test_buy_signal_on_stop_loss():
     
-    stop_loss_percentage = 20
-    trader = Trader(stop_loss_percentage)
-    close = 2000.0
-    min_ann = 1000.0
+    trader.position = min_ann
+    close = 1500.0
     is_last_candle_day= False
 
-    
+
     assert trader.buy_signal (close, min_ann, is_last_candle_day) == True
 
 def test_buy_signal_on_day_close():
     
-    stop_loss_percentage = 20
-    trader = Trader(stop_loss_percentage)
     close = 2000.0
-    min_ann = 1000.0
     is_last_candle_day= True
     
     assert trader.buy_signal (close, min_ann, is_last_candle_day) == True
 
 def test_sell_signal_on_sell():
     
-    stop_loss_percentage = 20
-    trader = Trader(stop_loss_percentage)
-    close = 2000.0
-    max_ann = 1000.0
+    close = 3200.0
     is_last_candle_day= False
     
     assert trader.sell_signal (close, max_ann, is_last_candle_day) == True
 
 def test_sell_signal_on_stop_loss():
     
-    stop_loss_percentage = 20
-    trader = Trader(stop_loss_percentage)
-    close = 1000.0
-    max_ann = 2000.0
+    trader.position = - max_ann
+    close = 3500.0
     is_last_candle_day= False
 
     
@@ -60,10 +50,8 @@ def test_sell_signal_on_stop_loss():
 
 def test_sell_signal_on_day_close():
     
-    stop_loss_percentage = 20
-    trader = Trader(stop_loss_percentage)
+    trader.position = - max_ann
     close = 1000.0
-    max_ann = 2000.0
     is_last_candle_day= True
     
     assert trader.sell_signal (close, max_ann, is_last_candle_day) == True
--- src/trader.py
+++ src/trader.py
@@ -7,18 +7,32 @@ class Trader(object):
     \item Comprar quando fechamento $\leq$ $min_{ANN}$, vender com preço de stop-loss
     """
 
-    def __init__(self, stop_loss):
+    def __init__(self, min_ann, max_ann, stop_loss):
         super(Trader, self).__init__()
-        self.stop_loss = stop_loss
+        self.stop_loss: float = stop_loss
         # self.initial_capital = initial_capital
         # self.total_capital = initial_capital
-        self.has_open_position = True
+        self.max_ann: float = max_ann
+        self.min_ann: float = min_ann
+        self.position: float = 0.0
 
-    def did_stop_loss_hit(self, close) -> bool:
+    def did_stop_loss_hit(self, close: float) -> bool:
+        alvo = self.max_ann - self.min_ann
+
+        if self.position > 0:
+            stop_loss_pontos = alvo * (self.stop_loss - 100)
+            stop_loss_posicao = self.position + stop_loss_pontos
+            return close < stop_loss_posicao
+        else:
+            stop_loss_pontos = alvo * (self.stop_loss - 100)
+            stop_loss_posicao = self.position - stop_loss_pontos
+            return close > stop_loss_posicao
+
+
         return False
 
-    def buy_signal(self, close, min_ann, is_last_candle_day) -> bool:
-        if ((close <= min_ann) or (is_last_candle_day and self.has_open_position) or (self.did_stop_loss_hit(close))):
+    def buy_signal(self, close: float, min_ann: float, is_last_candle_day: bool) -> bool:
+        if ((close <= min_ann) or (is_last_candle_day and self.position) or (self.did_stop_loss_hit(close))):
             return True
         return False
 
@@ -28,7 +42,7 @@ class Trader(object):
     \item Vender quando fechamento $\geq$ $max_{ANN}$, comprar com preço de stop-loss. 
     '''
 
-    def sell_signal(self, close, max_ann, is_last_candle_day) -> bool:
-        if close >= max_ann or (is_last_candle_day and self.has_open_position) or self.did_stop_loss_hit(close):
+    def sell_signal(self, close: float, max_ann: float, is_last_candle_day: bool) -> bool:
+        if close >= max_ann or (is_last_candle_day and self.position) or self.did_stop_loss_hit(close):
             return True
         return False
